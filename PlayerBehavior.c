9255d1768797063b0b6c5a80ab12aa6284f837e6
Tristan Begin <t.begin@digipen.edu>
2016-01-16 15:42:23 -0800

    Added Behavior Template

---
+++ PlayerBehavior.c
@@ -0,0 +1,43 @@
+#include "SoulEngine.h"
+
+static UNIT * pMyUnit;
+static TRANSFORM * pMyTransform;
+static ARCHETYPE * pMyArchetype;
+static LEVEL * pMyLevel;
+static GAME * pMyGame;
+static GAMESTATS * pMyGameStats;
+
+void Start();
+void Update();
+
+void PlayerBehavior(BEHAVIOR * Owner, char * Trigger)
+{
+  pMyUnit = Owner->pArchetype->pUnit;
+  pMyTransform = pMyUnit->pTransform;
+  pMyArchetype = pMyUnit->pArchetype;
+  pMyLevel = pMyUnit->pLevel;
+  pMyGame = pMyLevel->pGame;
+  pMyGameStats = pMyGame->pGameStats;
+
+  if (Trigger == "Start")
+  {
+    Start();
+  }
+
+  if (Trigger == "Update")
+  {
+    Update();
+  }
+}
+
+void Start()
+{
+  
+
+}
+
+void Update()
+{
+
+
+}
