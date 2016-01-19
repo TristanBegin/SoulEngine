fdcd7587723500565b685691f014ca0b0fab0bd8
Evan Collier <etc597@gmail.com>
2016-01-18 21:09:47 -0800

    Added MatrixMath, VectorMath, and Collision files with respective functions. SoulMath is now outdated and it will be deleted.

--- SoulMath.h
+++
@@ -1,7 +0,0 @@
-#include "SoulEngine.h"
-
-void GetNormalVec(VECTOR *pResult, const VECTOR *pVec);
-
-void GetNormal2Pts(VECTOR *pResult, const VECTOR *pPt0, const VECTOR *pPt1);
-
-void Project(VECTOR *pResult, const VECTOR *pVec0, const VECTOR *pVec1);
