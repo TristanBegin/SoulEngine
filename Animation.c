cac813bb42363e82315a70a3bf31bbe0dfa0a6a9
Stefan Lande <tetelande@gmail.com>
2016-01-22 15:53:59 -0800

    Animation Speed working. Animation done. GG.

--- Animation.c
+++ Animation.c
@@ -1,29 +1,36 @@
 #include "Animation.h"
-
+#include <time.h>
 void Animate(SPRITE * pSprite)
 {
-
 	
-	// If you are at the last frame of animation.
-	if ((pSprite->Offset.y + 1 / pSprite->RowCol.y >= 1) 
-		&& (pSprite->Offset.x + 1 / pSprite->RowCol.x >= 1))
+	if (pSprite->TimeSinceLastFrame > 1 / (5 *pSprite->AnimationSpeed))
 	{
-		// Go back to the first frame
-		pSprite->Offset.x = 0;
-		pSprite->Offset.y = 0;
+		pSprite->TimeSinceLastFrame = 0;
+		// If you are at the last frame of animation.
+		if ((pSprite->Offset.y + 1 / pSprite->RowCol.y >= 1)
+			&& (pSprite->Offset.x + 1 / pSprite->RowCol.x >= 1))
+		{
+			// Go back to the first frame
+			pSprite->Offset.x = 0;
+			pSprite->Offset.y = 0;
+		}
+		// If you are not at the end of the x. (so if x added with the frame size is < 1)
+		else if (pSprite->Offset.x + 1 / pSprite->RowCol.x < 1)
+		{
+			// Move the frame once to the right(x).
+			pSprite->Offset.x += 1 / pSprite->RowCol.x;
+		}
+		// If you are at the end of the x. (so if x added with the frame size is >= 1)
+		else
+		{
+			// Move the frame to the first column(x = 0)
+			pSprite->Offset.x = 0;
+			// Move the frame down(y) once
+			pSprite->Offset.y += 1 / pSprite->RowCol.y;
+		}
 	}
-	// If you are not at the end of the x. (so if x added with the frame size is < 1)
-	else if(pSprite->Offset.x + 1 / pSprite->RowCol.x < 1)
+	else
 	{
-		// Move the frame once to the right(x).
-		pSprite->Offset.x += 1 / pSprite->RowCol.x;
-	}
-	// If you are at the end of the x. (so if x added with the frame size is >= 1)
-	else 
-	{
-		// Move the frame to the first column(x = 0)
-		pSprite->Offset.x = 0;
-		// Move the frame down(y) once
-		pSprite->Offset.y += 1 / pSprite->RowCol.y;
+		pSprite->TimeSinceLastFrame += AEFrameRateControllerGetFrameTime();
 	}
 }
